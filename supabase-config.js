3400cf22d848216651023dfbdcb353dfcc617eff
MaqsatKendjebekov <116784211+MaqsatKendjebekov@users.noreply.github.com>
2026-06-05 13:49:44 -0700

    Enable Google OAuth button

--- supabase-config.js
+++ supabase-config.js
@@ -2,7 +2,7 @@ window.SUPABASE_CONFIG = {
   url: "https://ylfmnbvalusdctexehpv.supabase.co",
   anonKey: "sb_publishable_v1y89rEl7E8Cnm6akl2cdw_FwtQBN_z",
   oauth: {
-    google: false,
+    google: true,
     github: false
   }
 };
