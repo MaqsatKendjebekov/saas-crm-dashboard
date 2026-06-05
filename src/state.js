07b30e9c2e4b09667073bf361b137c501c44a20d
MaqsatKendjebekov <116784211+MaqsatKendjebekov@users.noreply.github.com>
2026-06-05 11:24:09 -0700

    Add src/state.js

---
+++ src/state.js
@@ -0,0 +1,40 @@
+export const views = {
+  dashboard: "Dashboard",
+  customers: "Customers",
+  deals: "Deals",
+  tasks: "Tasks",
+  billing: "Billing"
+};
+
+export const store = {
+  state: {
+    activeView: "dashboard",
+    globalSearch: "",
+    filters: {
+      customers: "All",
+      deals: "All",
+      tasks: "All",
+      billing: "All"
+    },
+    drawer: null,
+    theme: "dark",
+    toast: null
+  },
+  listeners: new Set(),
+  subscribe(listener) {
+    this.listeners.add(listener);
+    return () => this.listeners.delete(listener);
+  },
+  setState(updater) {
+    const nextState = typeof updater === "function" ? updater(this.state) : updater;
+    this.state = {
+      ...this.state,
+      ...nextState,
+      filters: {
+        ...this.state.filters,
+        ...(nextState.filters || {})
+      }
+    };
+    this.listeners.forEach((listener) => listener(this.state));
+  }
+};
